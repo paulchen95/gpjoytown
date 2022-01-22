43c718ee82d85d8d6b71a1250612fcc34e79ed00
paulchen95 <88908993+paulchen95@users.noreply.github.com>
2022-01-22 00:53:45 -0800

    Update index.php

--- index.php
+++ index.php
@@ -46,7 +46,7 @@
         <div style="height:720px; width:1280px; overflow: hidden;">
             <iframe width="1280"
                 src="https://www.youtube.com/embed/<?php echo htmlspecialchars($_GET['id']); 
-                    ?>?theme=dark&autoplay=1&autohide=0&cc_load_policy=1&modestbranding=1&fs=0&showinfo=0&rel=0&iv_load_policy=3&mute=0&loop=1"   
+                    ?>?theme=dark&autoplay=1&autohide=0&cc_load_policy=1&modestbranding=1&fs=1&showinfo=0&rel=0&iv_load_policy=3&mute=0&loop=1"   
                 style="height:720px;  background:#000000; bottom: 0px; position: relative;"  
                 sandbox="allow-forms allow-scripts allow-pointer-lock allow-same-origin allow-top-navigation"
                 allowfullscreen>
