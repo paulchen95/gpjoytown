db0fcafaa32f6b9a3e2936215f99982861f321d3
paulchen95 <88908993+paulchen95@users.noreply.github.com>
2022-01-22 00:23:29 -0800

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
     <head>
         <meta charset="utf-8">
         <title>Joytown Service</title>
-        <link rel="stylesheet" href="style.css">
+        <!-- <link rel="stylesheet" href="style.css"> -->
     </head>
     <body>
+        <!--
         <style>
             .hytPlayerWrap {
                 display: inline-block;
@@ -42,11 +43,22 @@
                 background-image: url(data:image/svg+xml;utf8;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZlcnNpb249IjEiIHdpZHRoPSIxNzA2LjY2NyIgaGVpZ2h0PSIxNzA2LjY2NyIgdmlld0JveD0iMCAwIDEyODAgMTI4MCI+PHBhdGggZD0iTTE1Ny42MzUgMi45ODRMMTI2MC45NzkgNjQwIDE1Ny42MzUgMTI3Ny4wMTZ6IiBmaWxsPSIjZmZmIi8+PC9zdmc+)
             }
         </style>
+        -->
+        <div style="height:400px; width:400px; overflow: hidden;">
+            <iframe width="464" height="316"
+                src="https://www.youtube.com/embed/<?php echo htmlspecialchars($_GET['id']); 
+                    ?>?theme=dark&autoplay=1&autohide=0&cc_load_policy=1&modestbranding=1&fs=0&showinfo=0&rel=0&iv_load_policy=3&mute=0&loop=1"   
+                style="height:400px;  background:#000000; bottom: 60px; position: relative;"  
+            sandbox="allow-forms allow-scripts allow-pointer-lock allow-same-origin allow-top-navigation">
+            </iframe>
+        </div>
+        <!--
         <div class="hytPlayerWrapOuter">
             <div class="hytPlayerWrap"> <iframe width="464" height="316"
-                    src="https://www.youtube.com/embed/<?php echo htmlspecialchars($_GET['id']); 
+                    src="https://www.youtube.com/embed/<?php //echo htmlspecialchars($_GET['id']); 
                     ?>?rel=0&enablejsapi=1&modestbranding=1&autohide=1&showinfo=0&controls=0" allowfullscreen frameborder="0"></iframe></div>
         </div>
         <script>"use strict"; document.addEventListener('DOMContentLoaded', function () { if (window.hideYTActivated) return; if (typeof YT === 'undefined') { let tag = document.createElement('script'); tag.src = "https://www.youtube.com/iframe_api"; let firstScriptTag = document.getElementsByTagName('script')[0]; firstScriptTag.parentNode.insertBefore(tag, firstScriptTag); } let onYouTubeIframeAPIReadyCallbacks = []; for (let playerWrap of document.querySelectorAll(".hytPlayerWrap")) { let playerFrame = playerWrap.querySelector("iframe"); let onPlayerStateChange = function (event) { if (event.data == YT.PlayerState.ENDED) { playerWrap.classList.add("ended"); } else if (event.data == YT.PlayerState.PAUSED) { playerWrap.classList.add("paused"); } else if (event.data == YT.PlayerState.PLAYING) { playerWrap.classList.remove("ended"); playerWrap.classList.remove("paused"); } }; let player; onYouTubeIframeAPIReadyCallbacks.push(function () { player = new YT.Player(playerFrame, { events: { 'onStateChange': onPlayerStateChange } }); }); playerWrap.addEventListener("click", function () { let playerState = player.getPlayerState(); if (playerState == YT.PlayerState.ENDED) { player.seekTo(0); } else if (playerState == YT.PlayerState.PAUSED) { player.playVideo(); } }); } window.onYouTubeIframeAPIReady = function () { for (let callback of onYouTubeIframeAPIReadyCallbacks) { callback(); } }; window.hideYTActivated = true; });</script>
+        -->
     </body>
 </html>
